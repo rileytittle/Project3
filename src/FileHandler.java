84c34d6a6aaa459d514640e27ccfa4ebe320131a
rileytittle <coletittle@ymail.com>
2023-04-04 21:40:30 -0400

    Create FileHandler class

---
+++ src/FileHandler.java
@@ -0,0 +1,24 @@
+import java.util.LinkedList;
+
+public class FileHandler 
+{
+	public void writeData(String workOrderFileName)
+	{
+		
+	}
+	
+	public void readEmployeeData(String employeeFileName)
+	{
+		
+	}
+	
+	public LinkedList<Ticket> readTicketData(String ticketFileName)
+	{
+		
+	}
+	
+	private void logger(String log)
+	{
+		
+	}
+}
