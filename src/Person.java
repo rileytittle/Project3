da439e3b48ecbceb3064e1d2d1b8f4299d2b5fb5
rileytittle <coletittle@ymail.com>
2023-04-04 20:53:41 -0400

    Create employee and person classes

---
+++ src/Person.java
@@ -0,0 +1,42 @@
+
+public class Person 
+{
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String phoneNumber;
+	private String email;
+
+	/**
+	 * Class Constructor
+	 * @param firstName first name of the person 
+	 * @param lastName last name of the person
+	 * @param address the address of the person
+	 * @param phoneNumber the phone number of the person
+	 * @param email the email of the person
+	 */
+	public Person(String firstName, String lastName, 
+			String address, String phoneNumber, String email) 
+	{
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+	}//end Person constructor
+	
+	/**
+	 * Returns a string with information about the person
+	 * @return a string with information about the person
+	 */
+	public String getFileData()
+	{
+		String data = "";
+		data += firstName + ",";
+		data += lastName + ",";
+		data += address + ",";
+		data += phoneNumber + ",";
+		data += email + ",";
+		return data;
+	}
+}//end class
--- src/Project3.java
+++ src/Project3.java
@@ -15,9 +15,10 @@ public class Project3
 	public static String tier1TicketFileName;
 	public static String tier2TicketFileName;
 	public static String workOrderFileName;
+	
 	public static ArrayList<Employee> employeeList = new ArrayList<Employee>();
-	public static Queue<Ticket> tier1TicketFile = new LinkedList<Ticket>();
-	public static Queue<Ticket> tier2TicketFile = new LinkedList<Ticket>();
+	public static Queue<Ticket> tier1TicketFile;
+	public static Queue<Ticket> tier2TicketFile;
 	public static ArrayList<WorkOrder> workOrderList = new ArrayList<WorkOrder>();
 	
 	public static void main(String[] args) 
