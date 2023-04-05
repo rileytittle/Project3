8567a6b1b9e4d62c60a984d603d5a61abf1acf19
rileytittle <coletittle@ymail.com>
2023-04-04 21:15:04 -0400

    Create Tier2Employee class

---
+++ src/Tier2Employee.java
@@ -0,0 +1,36 @@
+
+public class Tier2Employee extends Employee
+{
+	private String certification;
+	
+	/**
+	 * Constructor for Tier2Employee class
+	 * @param firstName
+	 * @param lastName
+	 * @param address
+	 * @param phoneNumber
+	 * @param email
+	 * @param employeeId
+	 * @param clockedIn
+	 * @param hiredDate
+	 * @param certification
+	 */
+	public Tier2Employee(String firstName, String lastName, String address,
+			             String phoneNumber, String email, String employeeId,
+			             String clockedIn, String hiredDate, String certification) 
+	{
+		super(firstName, lastName, address, phoneNumber, email, employeeId, clockedIn, hiredDate);
+		this.certification = certification;
+	}//end Tier2Employee constructor
+	
+	/**
+	 * Returns a string containing all the relevant data of a tier2employee object
+	 * @return a string containing all the relevant data of a tier2employee object
+	 */
+	public String getFileData()
+	{
+		String data = super.getFileData();
+		data += certification;
+		return data;
+	}
+}//end class
