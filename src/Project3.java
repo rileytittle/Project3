e50d08d607bee99addb96577dd824c7711d5c435
rileytittle <coletittle@ymail.com>
2023-04-04 20:41:02 -0400

    Create methods and variables for Project3 class

--- src/Project3.java
+++ src/Project3.java
@@ -1,8 +1,32 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
-public class Project3 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+/*
+ * Author: Tittle, Riley
+ * Course: COP3503
+ * Project #: 2
+ * Title: Input/Output
+ * Due Date: 3/26/2023
+ */
+public class Project3 
+{
+	public static String employeeFileName;
+	public static String tier1TicketFileName;
+	public static String tier2TicketFileName;
+	public static String workOrderFileName;
+	public static ArrayList<Employee> employeeList = new ArrayList<Employee>();
+	public static Queue<Ticket> tier1TicketFile = new LinkedList<Ticket>();
+	public static Queue<Ticket> tier2TicketFile = new LinkedList<Ticket>();
+	public static ArrayList<WorkOrder> workOrderList = new ArrayList<WorkOrder>();
+	
+	public static void main(String[] args) 
+	{
+		
+	}//end main method
+	
+	public static void createWorkOrders()
+	{
+		
 	}
-
-}
+}//end class
