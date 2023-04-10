8d74cd570fb6f86d89ab67d2217e8b1acdc39a22
rileytittle <coletittle@ymail.com>
2023-04-10 15:31:37 -0400

    Start readEmployeeData method

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,4 +1,7 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class FileHandler 
 {
@@ -9,7 +12,30 @@ public class FileHandler
 	
 	public void readEmployeeData(String employeeFileName)
 	{
+		FileReader fileReader = null;
+		System.out.println("Loading Employee Data");
+		try 
+		{
+			fileReader = new FileReader("employee_data.csv");
+		} 
+		catch (FileNotFoundException e) 
+		{
+			System.out.println(e);
+			e.printStackTrace();
+		}
+		Scanner fileDataIn = new Scanner(fileReader);
 		
+		fileDataIn.nextLine(); //scan in the headers to ignore them
+		
+		while(fileDataIn.hasNextLine())
+		{
+			String employeeData = fileDataIn.nextLine();
+			String[] employeeDataElements = employeeData.split(",");
+			if(employeeDataElements[9].equals(null))
+			{
+				
+			}
+		}
 	}
 	
 	public LinkedList<Ticket> readTicketData(String ticketFileName)
