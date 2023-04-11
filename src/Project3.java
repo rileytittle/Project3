ffbae9dc9d6767d3dc8c83c1c51b8258eec80220
rileytittle <coletittle@ymail.com>
2023-04-11 15:57:28 -0400

    recreate createWorkOrders method with new logic

--- src/Project3.java
+++ src/Project3.java
@@ -16,40 +16,62 @@ public class Project3
 	public static String tier1TicketFileName = "tier1_ticket_data.csv";
 	public static String tier2TicketFileName = "tier2_ticket_data.csv";
 	public static String workOrderFileName = "workorder_data.csv";
-	
+
 	public static ArrayList<Employee> employeeList = new ArrayList<Employee>();
 	public static Queue<Ticket> tier1TicketFile;
 	public static Queue<Ticket> tier2TicketFile;
 	public static ArrayList<WorkOrder> workOrderList = new ArrayList<WorkOrder>();
-	
+
 	public static void main(String[] args) 
 	{
 		System.out.println("Loading Employee Data");
-		
+
 		FileHandler.readEmployeeData(employeeFileName);
-		
+
 		System.out.println("Loading Ticket Data");
-		
+
 		tier1TicketFile = FileHandler.readTicketData(tier1TicketFileName);
 		tier2TicketFile = FileHandler.readTicketData(tier2TicketFileName);
-		
+
 		System.out.println("Creating Work Orders");
-		
+
 		createWorkOrders();
-		
+
 		System.out.println("Writing Work Order Data to File");
-		
+
 		FileHandler.writeData(workOrderFileName);
-		
+
 		System.out.println("Work Orders created. Program Exiting");
 	}//end main method
-	
+
 	public static void createWorkOrders()
 	{
-		
+
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm");
 		Date date = new Date();
-		
+		while(!(tier2TicketFile.isEmpty()))
+		{
+			for (Employee employee : employeeList) 
+			{
+				if(employee instanceof Tier2Employee)
+				{
+					WorkOrder workOrder = new WorkOrder(employee, tier2TicketFile.remove(), dateFormat.format(date));
+					workOrderList.add(workOrder);
+				}
+			}
+		}
+		while(!(tier1TicketFile.isEmpty()))
+		{
+			for (Employee employee : employeeList) 
+			{
+				if(!(employee instanceof Tier2Employee))
+				{
+					WorkOrder workOrder = new WorkOrder(employee, tier1TicketFile.remove(), dateFormat.format(date));
+					workOrderList.add(workOrder);
+				}
+			}
+		}
+		/*
 		int employeeCounter = 0;
 		for(int i = tier2TicketFile.size(); i >= 0; i--)
 		{
@@ -92,5 +114,6 @@ public class Project3
 				employeeCounter = 0;
 			}
 		}//end outer for loop
+		 */
 	}
 }//end class
