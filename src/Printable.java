636475b1d3a90fd6e762038df880eedc69f91762
rileytittle <coletittle@ymail.com>
2023-04-18 10:28:47 -0400

    Comments

--- src/Printable.java
+++ src/Printable.java
@@ -1,4 +1,8 @@
-
+/**
+ * Printable interface has getFileData method that 
+ * should return all the relevant data about an object
+ * in a comma-separated format
+ */
 public interface Printable 
 {
 	String getFileData();
